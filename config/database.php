4cf1840d6af31d8fe623ebd1c029deea4d91d7b3
icekang <namkang_chutisilp@hotmail.com>
2020-11-22 11:24:34 +0700

    🐋 Dockerize app

--- config/database.php
+++ config/database.php
@@ -52,6 +52,7 @@ return [
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
+            'port' => env('DB_PORT', 3306),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
